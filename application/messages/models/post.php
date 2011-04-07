ed73157507dc0c5f3f6fba76faed379078b37ee7
yoshi labarda <aqlx86@gmail.com>
2011-04-07 18:42:04 +0800

    added post messages

---
+++ application/messages/models/post.php
@@ -0,0 +1,11 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+return array(
+	'title' => array(
+		'not_empty' => 'title cannot be empty',
+		'min_length' => 'minimum lenght for title is :param2 characters'
+	),
+	'content' => array(
+		'not_empty' => 'content cannot be empty'
+	)
+);
