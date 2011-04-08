10629877cdbee35cd7690495663a53a1ffcc661c
yoshi labarda <aqlx86@gmail.com>
2011-04-08 13:24:46 +0800

    added database config.

---
+++ application/config/development/database.php
@@ -0,0 +1,33 @@
+<?php defined('SYSPATH') or die('No direct access allowed.');
+
+return array
+(
+	'default' => array
+	(
+		'type'       => 'mysql',
+		'connection' => array(
+			'hostname'   => 'localhost',
+			'database'   => 'kohanatest',
+			'username'   => 'root',
+			'password'   => '',
+			'persistent' => FALSE,
+		),
+		'table_prefix' => '',
+		'charset'      => 'utf8',
+		'caching'      => FALSE,
+		'profiling'    => TRUE,
+	),
+	'alternate' => array(
+		'type'       => 'pdo',
+		'connection' => array(
+			'dsn'        => 'mysql:host=localhost;dbname=kohana',
+			'username'   => 'root',
+			'password'   => 'r00tdb',
+			'persistent' => FALSE,
+		),
+		'table_prefix' => '',
+		'charset'      => 'utf8',
+		'caching'      => FALSE,
+		'profiling'    => TRUE,
+	),
+);
