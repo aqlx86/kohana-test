7a08ed0da4e18dadbc04db08f14eed5de1758d9a
arnel labarda <aqlx86@gmail.com>
2011-04-21 14:38:25 +0800

    updated layouts

--- application/classes/view/layout.php
+++ application/classes/view/layout.php
@@ -2,11 +2,59 @@
 
 class View_Layout extends Kostache_Layout {
 	
+	public  $_errors = array();
+	
 	protected $_layout = 'layout/default';
 	
 	protected $_partials = array(
 		'footer' => 'layout/footer'
 	);
+	
+	public function assets_body()
+	{
+		$assets = '';
+		
+		foreach (Assets::get('body') as $asset)
+		{
+			$assets .= $asset;
+		}
+
+		return $assets;
+	}
+	
+	public function assets_head()
+	{
+		$assets = '';
+		
+		foreach (Assets::get('head') as $asset)
+		{
+			$assets .= $asset."\n";
+		}
+
+		return $assets;
+	}
+	
+	public function base_url()
+	{
+		return URL::base(TRUE);
+	}
+	
+	public function errors()
+	{
+		$e = array();
+		
+		foreach($this->_errors as $key => $error)
+		{
+			$e[] = array(
+				'field' => $key, 'message' => $error
+			);
+		}
+		
+		if(count($e) < 1)
+			return;
+		
+		return array('messages' => $e);
+	}
 	
 	public function render()
 	{
--- application/classes/view/page.php
+++ application/classes/view/page.php
@@ -2,54 +2,13 @@
 
 class View_Page extends View_Layout {
 	
-	public  $_errors = array();
+	public $_user;
 	
 	public function _initialize()
 	{
 		Assets::add_group('default-template');
 	
 		parent::_initialize();
-	}
-	
-	public function assets_body()
-	{
-		$assets = '';
-		
-		foreach (Assets::get('body') as $asset)
-		{
-			$assets .= $asset;
-		}
-
-		return $assets;
-	}
-	
-	public function assets_head()
-	{
-		$assets = '';
-		
-		foreach (Assets::get('head') as $asset)
-		{
-			$assets .= $asset."\n";
-		}
-
-		return $assets;
-	}
-	
-	public function errors()
-	{
-		$e = array();
-		
-		foreach($this->_errors as $key => $error)
-		{
-			$e[] = array(
-				'field' => $key, 'message' => $error
-			);
-		}
-		
-		if(count($e) < 1)
-			return;
-		
-		return array('messages' => $e);
 	}
 	
 	public function render()
